c34a1184c9f374548ee711596bfc1f097c3fe171
Lauren Libretti <lalibretti@yahoo.com>
2023-09-20 16:25:47 -0400

    Filled out from class 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
+import Header from './components/Header.jsx';
+import HomePage from './pages/HomePage.jsx';
+import FeedItem from './components/FeedItem';
+import FeedList from './components/FeedList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <HomePage />
+            
+        </div>
+    );
 }
 
 export default App;
